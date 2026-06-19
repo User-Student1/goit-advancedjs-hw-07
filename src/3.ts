f1ba547173355b907f842d201faa0979c98b58b8
User-Student1 <illiaserhunin@gmail.com>
2026-06-19 13:31:11 +0000

    HW-7

--- src/3.ts
+++ src/3.ts
@@ -8,11 +8,20 @@
 */
 
 // реалізація класу Wizard
+interface ICharacter {
+  name: string;
+  level: number;
+  introduce(phrase: string): void;
+  levelUp(): void;
+}
+
+interface ISpellCaster {
+  castSpell(): void;
+}
+
 class Wizard implements ICharacter, ISpellCaster {
   constructor(public name: string, public level: number) {
-    if (this.level < 1) {
-      throw new Error('Level too low');
-    }
+    if (this.level < 1) throw new Error('Level too low');
   }
 
   introduce(phrase: string): void {
--- src/4.ts
+++ src/4.ts
@@ -1,10 +1,37 @@
-const key = new Key();
+class Key {
+    private signature: number = Math.random();
+    getSignature(): number { return this.signature; }
+}
 
+class Person {
+    constructor(private key: Key) {}
+    getKey(): Key { return this.key;}
+}
+
+abstract class House {
+    protected door: boolean = false;
+    protected tenants: Person[] = [];
+    constructor(protected key: Key) {}
+
+    comeIn(person: Person): void {
+        if (this.door) this.tenants.push(person);
+    }
+    abstract openDoor(key: Key): void;
+}
+
+class MyHouse extends House {
+    openDoor(key: Key): void {
+        if (key.getSignature() === this.key.getSignature()) {
+            this.door = true;
+        }
+    }
+}
+
+const key = new Key();
 const house = new MyHouse(key);
 const person = new Person(key);
 
 house.openDoor(person.getKey());
-
 house.comeIn(person);
 
 
